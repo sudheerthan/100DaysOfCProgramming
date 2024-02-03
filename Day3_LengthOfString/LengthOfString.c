23e57b64c623796f3af3f9bc11dbf043c8d1914b
sudheerthan <sudeerthan@gmail.com>
2024-02-03 23:52:27 +0530

    error codition added

--- Day3_LengthOfString/LengthOfString.c
+++ Day3_LengthOfString/LengthOfString.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int findLen(char str[]);
+void getInput(char str[],int len);
 
 int main(){
 	char str[50];
-	printf("Enter a string\n");
-	scanf("%s",str);
+
+	getInput(str,49);
+
 	int len = findLen(str);
-	printf("the lenth of string = %d\n", len);
+
+	if(len >= 49 && str[49] != '\n'){
+		printf("error: may have been string is truncated");
+	}
+	printf("the lenth of string = %d\n", len); // len-1 is to avoid '\0'
 }
 
 int findLen(char str[]){
 	int len =0;
-	while(str[len]!='\0'){
+	while(str[len] !='\0' && str[len] != '\n'){
 		len++;
 	}
 	return len;
 }
+
+void getInput(char str[],int size){
+	printf("Enter a string\n");
+	if(fgets(str,size,stdin)==NULL){
+		printf("Error reading input\n");
+		exit(1);
+	}
+	int len = findLen(str);
+	//remove newline if present
+	if(len >0 && str[len-1] == '\n'){
+		str[len-1]='\0';
+	}
+}
+	
+	

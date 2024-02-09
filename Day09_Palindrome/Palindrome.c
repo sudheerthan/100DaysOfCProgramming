f88063593d5db5d48fff4f1723a5a4ed5a44116c
sudheerthan <sudeerthan@gmail.com>
2024-02-09 23:51:46 +0530

    Update Palindrome.c

--- Day09_Palindrome/Palindrome.c
+++ Day09_Palindrome/Palindrome.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main(){
-    int og_num,num;
-    printf("enter a number\n");
-    scanf("%d",&og_num);
-    
+int main() {
+    int num, reversed = 0, original, temp;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    original = num;
+    while (num != 0) {
+        temp = num % 10;
+        reversed = reversed * 10 + temp;
+        num /= 10;
+    }
+    if (original == reversed)
+        printf("The number is a palindrome number.");
+    else
+        printf("The number is not a palindrome number.");
+    return 0;
 }

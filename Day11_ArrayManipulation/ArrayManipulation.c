b278f60cd451fa120e1d20fa1f6761458feabff8
sudheerthan <sudeerthan@gmail.com>
2024-02-15 23:24:02 +0530

    update code

--- Day11_ArrayManipulation/ArrayManipulation.c
+++ Day11_ArrayManipulation/ArrayManipulation.c
@@ -3,7 +3,16 @@
 #define MAX_SIZE 100
 
 void insertElement(int arr[],int *size, int element, int position){
-    if(*size >= MAX_SIZE )
+    if(*size >= MAX_SIZE ){
+        printf("array is full, insertion failed");
+        return 1;
+    }
+    if(position < 0 || position > MAX_SIZE){
+        printf("invalid postion , insertion failed");
+    }
+    for(int i = *size;i > position; i--){
+        arr[i] = arr[i-1]
+    }
 }
 
 int main(){

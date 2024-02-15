e252dd9e155329c1186c6ea170877794a92c3e7e
sudheerthan <sudeerthan@gmail.com>
2024-02-15 23:01:16 +0530

    update code

---
+++ Day11_ArrayManipulation/ArrayManipulation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+void insertElement(int arr[],int *size, int element, int position){
+    if(*size >= MAX_SIZE )
+}
+
+int main(){
+    int arr[MAX_SIZE];
+    int size = 0;
+
+
+}
